d4166c9a1b92b1ee2823d33e61329370674c6241
Jorge Feliz <jorgefelizv@gmail.com>
2021-07-16 20:46:59 -0400

    entry ready

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("NewPage/", views.NewPage, name="NewPage"),
+    path("wiki/<str:title>", views.wiki, name="wiki")
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,42 @@
 from django.shortcuts import render
+from django import forms
+from django.http import HttpResponse
 
 from . import util
 
+class wikiForm(forms.Form):
+    title = forms.CharField(label="Title")
+  
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+
+def NewPage(request):
+    if request.method == "POST":
+        
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        #form = wikiForm(request.POST)
+        
+        #if form.is_valid():
+          #  title = form.cleaned_data["Title"]
+       
+        entries = util.list_entries()
+        for entry in entries:
+            if entry.lower() == title.lower():
+                return render(request, "encyclopedia/EntryError.html")
+
+        util.save_entry(title, content)
+
+        return render(request, "encyclopedia/index.html")
+
+    else:
+        return render(request, "encyclopedia/NewPage.html")
+
+
+def wiki(request, title):
+    return HttpResponse("Hello, {title}")
+    
